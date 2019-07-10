385d53f83d10d0ec0f122111da5d8b5000179df6
geek-sajjad <sajjad.sohrabi2@gmail.com>
2019-07-10 15:16:53 +0430

    added ui to register page

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:stock_app/pages/register_page.dart';
 
 import './pages/splash_screen.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+//  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+//
+//  ));
+  SystemChrome.setEnabledSystemUIOverlays([]);
+
+}
+
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -12,9 +22,10 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
+        fontFamily: "Rubik",
         primarySwatch: Colors.purple,
       ),
-      home: SplashScreen(),//MyHomePage(title: 'Stock Management App'),
+      home: RegisterPage(),//MyHomePage(title: 'Stock Management App'),
     );
   }
 }
